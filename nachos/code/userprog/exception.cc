4c5b41b2a8a17e6f72299e444a6f262b25ea249d
shibhansh <shibhanshdohare1997@gmail.com>
2016-09-25 22:05:10 +0530

    add/numberOfInstructions

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -249,6 +249,12 @@ ExceptionHandler(ExceptionType which)
 		machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
 		machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);		
 	}
+	else if ((which == SyscallException) && (type == SYScall_NumInstr)) {
+		machine->WriteRegister(2, currentThread->numInstr());
+		machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+		machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+		machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+	}
 	else if ((which == SyscallException) && (type == SYScall_GetPA)) {
 		int virtAddr = machine->ReadRegister(4);
 		unsigned int vpn,offset,pageFrame;
